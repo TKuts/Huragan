b9b32d22884b79d6e7b1ad26cdc6e475dbbc8b03
Anton Kuts <anton.a.kuts@gmail.com>
2023-11-12 20:57:49 +0200

    completed first lesson js

---
+++ Lesson1/startApplication.js
@@ -0,0 +1,29 @@
+/*
+Number type 75
+String type "Some string"
+BigInt type 4925250777218564n
+Boolean type True/False
+Null type tull
+Undefined type Undefined
+Object type {x: 34, y: 69}
+Symbol type id  
+*/
+
+const name = "Max";
+
+console.log(name);
+
+const Log = {
+  log: function () {
+    return 1 + 3;
+  },
+};
+
+console.log(Log.log());
+
+const people = {
+  name: "Max",
+  age: 21,
+};
+
+console.log(people.age);
